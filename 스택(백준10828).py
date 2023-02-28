b0957e94af5045749eb0fec2128108aa4255f77e
jihyeon33 <twinhks1@naver.com>
2023-02-28 23:40:14 +0900

    2023.2.28

---
+++ 스택(백준10828).py
@@ -0,0 +1,26 @@
+import sys
+
+n=int(sys.stdin.readline())
+stack=[]
+for i in range(n):
+    do=sys.stdin.readline()
+    dosplit= do.split()
+    do1 =dosplit[0]
+    if len(dosplit)>1:
+        do2=dosplit[1]
+    if do1 =="push":
+        stack.append(do2)
+    elif do1=="pop":
+        if len(stack)==0:
+            print("-1")
+        else:
+            print(stack.pop())
+    elif do1=="size":
+        print(len(stack))
+    elif do1=="empty":
+        print("1" if len(stack)==0 else "0")
+    elif do1=="top":
+        if len(stack) ==0:
+            print("-1")
+        else:
+            print(stack[-1])
