324b261e7627e33cb897fb618052e219b5ca5013
jihyeon33 <twinhks1@naver.com>
2023-05-01 17:54:26 +0900

    2023.05.01

--- java/algorithm/src/algorithm/ex1.java
+++ java/algorithm/src/algorithm/ex1.java
@@ -2,7 +2,7 @@ package algorithm;
 import java.util.*;
 
 public class ex1 {
-/*백준 11724 연결요소의 개수  dfs*/
+/*백준 11724 연결요소의 개수  dfs  성공정답*/
 	static ArrayList<Integer>[] gp;
 	static boolean[] visted;
 	static int cnt=0;
@@ -21,18 +21,19 @@ public class ex1 {
 			gp[a].add(b);
 			gp[b].add(a);
 		}
-		System.out.print(gp[1]);
 		for (int i=1;i<=n; i++) {
 			if(visted[i]!= true) {
 				cnt++;
 				dfs(i);
 			}
 		}
+		System.out.print(cnt);
 	}
 	public static void dfs(int v) {
 		if (visted[v]==true) {
 			return; 
 		}
+		visted[v]=true;
 		for(int i: gp[v]) {
 			if(visted[i]!=true) {
 				dfs(i);
