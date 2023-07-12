d8650e21c9d0465d0eb82895226ba8ec9ad6af57
jihyeon33 <twinhks1@naver.com>
2023-07-12 17:35:20 +0900

    2023.7.12

--- java/algorithm/src/algorithm/ex1717.java
+++ java/algorithm/src/algorithm/ex1717.java
@@ -3,6 +3,7 @@ package algorithm;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.ArrayList;
 import java.util.StringTokenizer;
 
 /*백준 1717 집합의 표현*/
@@ -10,14 +11,24 @@ public class ex1717 {
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st = new StringTokenizer(br.readLine());
-		int a = Integer.parseInt(st.nextToken());
-		int b = Integer.parseInt(st.nextToken());
-		st = new StringTokenizer(br.readLine());  //다음라인을 읽을때는 st 초기화
-		int c = Integer.parseInt(st.nextToken());
+		int n = Integer.parseInt(st.nextToken());
+		int m = Integer.parseInt(st.nextToken());
+		ArrayList<Integer>[] list;
+		list = new ArrayList[m+1];
+		for(int i =1; i<=m ; i++) {
+			list[i]= new ArrayList<Integer>();
+		}
+		for (int i=1; i<=m ;i++) {
+			st = new StringTokenizer(br.readLine());  //다음라인을 읽을때는 st 초기화
+			list[i].add(Integer.parseInt(st.nextToken()));
+			list[i].add(Integer.parseInt(st.nextToken()));
+			list[i].add(Integer.parseInt(st.nextToken()));
+		}
 		
-		System.out.println(a);
-		System.out.println(b);
-		System.out.println(c);
+		for(int i =1; i<=m ;i++) {
+			System.out.println(list[i]);
+		}
+
 	}
 
 }
